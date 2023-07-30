01a09bd9f18c9897fe2725d3eb9c6effd4c3aa1b
Bin <k1234988@gmail.com>
2023-07-30 19:46:24 +0800

    first

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+    internal class Program : TEST {
+
+        static void Main(string[] args) {
+         Stack<char> v = new Stack<char>();
+            v.Push('{');
+            Console.WriteLine('{'!=v.Pop());
+            Console.WriteLine( v.Pop());
+
+            Console.ReadLine();
+          
+        }
+    }
+}
